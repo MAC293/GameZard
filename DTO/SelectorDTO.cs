37ec292cac6ecf0699126f8af0a1cafabb8c3841
José Pino <mac110973@gmail.com>
2025-07-26 21:35:56 -0400

    Go back to README name

--- DTO/SelectorDTO.cs
+++ DTO/SelectorDTO.cs
@@ -1,67 +1,67 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using GameZard.Context;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using CommunityToolkit.Mvvm.ComponentModel;
+//using GameZard.Context;
+//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.ComponentModel.DataAnnotations;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace GameZard.DTO
-{
-    public partial class SelectorDTO : ObservableObject
-    {
-        [ObservableProperty]
-        private String _Name;
+//namespace GameZard.DTO
+//{
+//    public partial class SelectorDTO : ObservableObject
+//    {
+//        [ObservableProperty]
+//        private String _Name;
 
-        [ObservableProperty]
-        private Byte[] _Icon;
+//        [ObservableProperty]
+//        private Byte[] _Icon;
 
-        [ObservableProperty]
-        private String _Console;
+//        [ObservableProperty]
+//        private String _Console;
 
-        [ObservableProperty]
-        private String _ExecutableLocation;
+//        [ObservableProperty]
+//        private String _ExecutableLocation;
 
-        [ObservableProperty]
-        private ObservableCollection<String> _Emulators;
+//        [ObservableProperty]
+//        private ObservableCollection<String> _Emulators;
 
-        public SelectorDTO()
-        {
-            Emulators = new ObservableCollection<String>();
-        }
+//        public SelectorDTO()
+//        {
+//            Emulators = new ObservableCollection<String>();
+//        }
 
-        public String Name
-        {
-            get { return _Name; }
-            set { _Name = value; }
-        }
+//        public String Name
+//        {
+//            get { return _Name; }
+//            set { _Name = value; }
+//        }
 
-        public Byte[] Icon
-        {
-            get { return _Icon; }
-            set { _Icon = value; }
-        }
+//        public Byte[] Icon
+//        {
+//            get { return _Icon; }
+//            set { _Icon = value; }
+//        }
 
-        public String Console
-        {
-            get { return _Console; }
-            set { _Console = value; }
-        }
+//        public String Console
+//        {
+//            get { return _Console; }
+//            set { _Console = value; }
+//        }
 
-        public String ExecutableLocation
-        {
-            get { return _ExecutableLocation; }
-            set { _ExecutableLocation = value; }
-        }
+//        public String ExecutableLocation
+//        {
+//            get { return _ExecutableLocation; }
+//            set { _ExecutableLocation = value; }
+//        }
 
-        public ObservableCollection<String> Emulators
-        {
-            get { return _Emulators; }
-            set { _Emulators = value; }
-        }
+//        public ObservableCollection<String> Emulators
+//        {
+//            get { return _Emulators; }
+//            set { _Emulators = value; }
+//        }
 
 
-    }
-}
+//    }
+//}
