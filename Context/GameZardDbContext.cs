d2d0c055ac3669d2522dddf57ecfddcad8ca8038
Joseph Pino <mac110973@gmail.com>
2025-11-16 14:05:58 -0300

    Latest change made before retaking after a hiatus

--- Context/GameZardDbContext.cs
+++ Context/GameZardDbContext.cs
@@ -25,7 +25,7 @@ public partial class GameZardDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=C:\\Users\\Joseph Pino\\Desktop\\Backup\\GameZard Project\\GameZard\\Database\\GameZard.db");
+        => optionsBuilder.UseSqlite("Data Source=D:\\Programming\\Business Projects\\GameZard Project\\GameZard\\Database\\GameZard.db");
 
     //C:\Users\Joseph Pino\Desktop\Backup\GameZard Project\GameZard\Database
 
