b45832ea3ea94ab3512d558bcf2595d002b5cd7b
Joseph Pino <mac110973@gmail.com>
2025-12-27 22:06:01 -0300

    Emulator removal is done

--- ViewModels/EmulatorViewModels/EmulatorViewModel.cs
+++ ViewModels/EmulatorViewModels/EmulatorViewModel.cs
@@ -19,16 +19,7 @@ namespace ViewModels.EmulatorViewModels
 
         public EmulatorViewModel()
         {
-            //ELVM = new EmulatorListViewModel();
 
-            //WeakReferenceMessenger.Default.Register<EmulatorSelectedMessage>(this, (recipient, message) =>
-            //{
-            //    if (message.Emulator != null)
-            //    {
-            //        ELVM.ListDomain.EmulatorDomain.Emulator = message.Emulator;
-            //        ELVM.ListDomain.LoadEmulators();
-            //    }
-            //});
         }
 
         public EmulatorListViewModel ELVM
