22a6a42e89755c299e2da190fb1d633eba36046c
Joseph Pino <mac110973@gmail.com>
2026-01-26 20:46:48 -0300

    Last Save added

--- Scripts/Queries.sql
+++ Scripts/Queries.sql
@@ -25,4 +25,8 @@ UPDATE Emulator_Savedata
 SET To_Path = 'D:\Videogames\Windows\Resident Evil 2\NoDVD\Goldberg\steam_settings'
 WHERE ID = 'Save_PPSSPP';
 
+UPDATE Emulator_Savedata
+SET Last_Save = '01/04/2021 at 13:00'
+WHERE ID = 'Save_PPSSPP';
+
 
