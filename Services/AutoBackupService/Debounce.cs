024313a15821291635faf0348e2f7242b7adc567
Joseph Pino <mac110973@gmail.com>
2026-03-21 17:13:42 -0300

    Before adding Debouncer to Watcher

--- Services/AutoBackupService/Debounce.cs
+++ Services/AutoBackupService/Debounce.cs
@@ -13,47 +13,36 @@ namespace GameZard.Services.AutoBackupService
 {
     public class Debounce : IDisposable
     {
-        private Dictionary<String, Timer> _Timers;
-
-        public Debounce()
-        {
-            Timers = new Dictionary<String, Timer>();
-        }
-
-        public Dictionary<String, Timer> Timers
-        {
-            get { return _Timers; }
-            set { _Timers = value; }
-        }
+        private readonly Dictionary<String, Timer> _timers = new();
 
         public void Execute(String key, int delayMilliseconds, Action action)
         {
-            if (Timers.ContainsKey(key))
+            if (_timers.ContainsKey(key))
             {
-                Timers[key].Stop();
-                Timers[key].Dispose();
-                Timers.Remove(key);
+                _timers[key].Stop();
+                _timers[key].Dispose();
+                _timers.Remove(key);
             }
 
             var timer = new Timer(delayMilliseconds);
-            timer.AutoReset = false;
+            timer.AutoReset = false; 
             timer.Elapsed += (s, e) =>
             {
                 action();
-                Timers.Remove(key);
+                _timers.Remove(key);
                 timer.Dispose();
             };
 
-            Timers[key] = timer;
+            _timers[key] = timer;
             timer.Start();
         }
 
         public void Dispose()
         {
-            foreach (var timer in Timers.Values)
+            foreach (var timer in _timers.Values)
                 timer.Dispose();
 
-            Timers.Clear();
+            _timers.Clear();
         }
     }
 }
