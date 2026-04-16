3a58f5041fc33235aa5cd88a7037c7f2c7942588
Joseph Pino <mac110973@gmail.com>
2026-04-15 22:26:40 -0400

    Relative path added

--- Context/GameZardDbContext.cs
+++ Context/GameZardDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using Avalonia.Controls.Shapes;
 using Microsoft.EntityFrameworkCore;
+using System.IO;
+
 
 namespace GameZard.Context;
 
@@ -23,9 +26,23 @@ public partial class GameZardDbContext : DbContext
 
     public virtual DbSet<Videogame> Videogames { get; set; }
 
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseSqlite("Data Source=D:\\Programming\\Business Projects\\GameZard Project\\GameZard\\Database\\GameZard.db");
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=D:\\Programming\\Business Projects\\GameZard Project\\GameZard\\Database\\GameZard.db");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // Base directory where the app is running
+            var basePath = AppContext.BaseDirectory;
+
+            // Build relative path to your Database folder inside the project
+            var dbPath = System.IO.Path.Combine(basePath, "Database", "GameZard.db");
+
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
